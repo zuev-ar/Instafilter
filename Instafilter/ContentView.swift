7627b4caf5ea578eb7699aec91b00ea11ce54a0a
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-06-25 17:05:12 +0500

    project 13, part 2, Integrating Core Image with SwiftUI

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView1: View {
     @State private var blurAmout: CGFloat = 0
@@ -29,7 +31,7 @@ struct ContentView1: View {
     }
 }
 
-struct ContentView: View {
+struct ContentView2: View {
     @State private var showingActionSheet = false
     @State private var backgroundColor = Color.white
     
@@ -48,6 +50,50 @@ struct ContentView: View {
                     .cancel()
                 ])
             }
+    }
+}
+
+struct ContentView: View {
+    @State private var image: Image?
+    
+    var body: some View {
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        guard let inputImage = UIImage(named: "Example") else { return }
+        
+        let begineImage = CIImage(image: inputImage)
+        let context = CIContext()
+        
+//        let currentFilter = CIFilter.sepiaTone()
+//        currentFilter.inputImage = begineImage
+//        currentFilter.intensity = 1
+        
+//        let currentFilter = CIFilter.pixellate()
+//        currentFilter.inputImage = begineImage
+//        currentFilter.scale = 100
+        
+//        let currentFilter = CIFilter.crystallize()
+//        currentFilter.inputImage = begineImage
+//        currentFilter.radius = 100
+        
+        guard let currentFilter = CIFilter(name: "CITwirlDistortion") else { return }
+        currentFilter.setValue(begineImage, forKey: kCIInputImageKey)
+        currentFilter.setValue(2000, forKey: kCIInputRadiusKey)
+        currentFilter.setValue(CIVector(x: inputImage.size.width / 2, y: inputImage.size.height / 2), forKey: kCIInputCenterKey)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgImage)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
