73836324fb4431147e126618bce8c1359a9f8c30
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-06-27 23:16:48 +0500

    project 13, part 3, How to save images to the user's photo library

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -53,6 +53,16 @@ struct ContentView2: View {
     }
 }
 
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+    
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save finished!")
+    }
+}
+
 struct ContentView: View {
     @State private var image: Image?
     @State private var showingImagePicker = false
@@ -76,6 +86,8 @@ struct ContentView: View {
     func loadImage() {
         guard let inputImage = inputImage else { return }
         image = Image(uiImage: inputImage)
+        let imageSaver = ImageSaver()
+        imageSaver.writeToPhotoAlbum(image: inputImage)
     }
 }
 
